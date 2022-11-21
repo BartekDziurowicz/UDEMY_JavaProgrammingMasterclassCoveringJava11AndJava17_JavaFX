7ba06c389ea211c6c8615e8deba18a4929fec152
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-11-21 17:15:46 +0100

    13.193 Events and Event Handlers

---
+++ JavaFX/src/main/java/com/example/javafx/Controller.java
@@ -0,0 +1,14 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField nameField;
+
+    public void onButtonClicked() {
+        System.out.println("Hello, " + nameField.getText());
+    }
+
+}
