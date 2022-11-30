cdc27a783b140c43d1577763d78860587ef3ffff
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-11-30 17:50:09 +0100

    13.214 Threads and Runnable

--- JavaFX/src/main/java/com/example/javafx/Controller.java
+++ JavaFX/src/main/java/com/example/javafx/Controller.java
@@ -1,9 +1,11 @@
 package com.example.javafx;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -13,9 +15,10 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
-
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     public void initialize() {
         helloButton.setDisable(true);
@@ -29,11 +32,28 @@ public class Controller {
             System.out.println("Bye "+nameField.getText());
         }
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
-        }
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = Platform.isFxApplicationThread() ? "UI thread" : "background thread";
+                    System.out.println(s);
+                    Thread.sleep(3000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI thread" : "background thread";
+                            System.out.println(s);
+                            ourLabel.setText("Text changed");
+                        }
+                    });
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        };
+
+        new Thread(task).start();
 
         if(ourCheckBox.isSelected()) {
             nameField.clear();
